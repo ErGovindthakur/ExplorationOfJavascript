78e41d1a6e3d814cca9a9173c796b14b281ca98a
ErGovindthakur <142775466+ErGovindthakur@users.noreply.github.com>
2024-01-20 21:28:04 +0000

    understanding of variable and constant

---
+++ 01_basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 12234;
+let name = "Govind";
+var emailId = "email@gmail.com";
+cityName = "Jaipur";
+console.log(accountId); //Output = 12234
+//accountId = 2222;
+//console.log(accountId);
+ /* not allowed bcz 
+a constant value can't changed*/
+cityName = "Bengaluru";
+console.table([accountId,name,emailId,cityName]);
+/*
+Don't prefer to use var in your code must of time
+*/
