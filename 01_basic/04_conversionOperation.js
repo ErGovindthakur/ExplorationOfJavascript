79799899e5ac6cbe38a05ad29862f7fcaf280766
ErGovindthakur <142775466+ErGovindthakur@users.noreply.github.com>
2024-01-21 19:39:03 +0000

    Learning operation in js

--- 01_basic/04_conversionOperation.js
+++ 01_basic/04_conversionOperation.js
@@ -4,36 +4,47 @@ console.log(typeof data); // output = number ;
 data = "33";
 console.log(typeof(data)); //output = String
 
+/*
 let convert = Number(data);
 console.log(typeof convert); //output = number
 console.log(convert);
+*/
 
+/*
 let another = "33";
 console.log(typeof another); //output = string
 let tocheck = Number(another);
 console.log(typeof tocheck);
+*/
 
+/*
 let explore = "33abc";
 console.log(explore);
 let toconvert = Number(explore);
 console.log(typeof toconvert);
 console.log(toconvert);
+*/
 
+/*
 let undef = undefined;
 console.log(undef);
 console.log(typeof undef); //output = undefined
- 
+*/
+
+/* 
 let empty = null;
 console.log(empty);
 console.log(typeof empty); // output = object
 let emptycheck = Number(empty);
 console.log(empty); //output = 0
 console.log(typeof empty); //output  = object
- 
-let boolean = true;
+ */
+
+/*let boolean = true;
 console.log(boolean);
 let num = Number(boolean);
 console.log(typeof boolean);
+*/
 
 /*Conversion notes
 "33" => 33
@@ -56,3 +67,41 @@ let string = String(exploration);
 console.log( typeof string);
 let bool = Boolean(exploration);
 console.log(typeof bool);
+
+
+// *************************** Operation **************************
+let number = 6;
+let neg = -number;
+console.log(neg); // output = -6
+
+// All the code which are written below all are performing an operation
+/*
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(3%3);
+*/
+
+let greet = "Hello ";
+let name = "Ajay";
+let both = greet + name;
+// console.log(both);
+let bothAgain = greet - name;
+//console.log(bothAgain); // output = NaN and it's not allowed
+
+// Let's do something cool
+
+/*
+console.log(1 + "2"); //output = 12
+console.log("1" + 2); //output = 12
+console.log(1 + 2 + "2"); //output = 32
+*/
+
+// Don't write code like that 
+console.log(3+4*2-1);
+console.log((3+4)*2-1);
+
+//Do fun with 
+console.log(+true);
+//console.log(+flase); not allowed
